0b2c9c2fafa8ae35d6da1214c02f8ddd6220fcff
Vũ Đức Thắng <125382939+Thangvu21@users.noreply.github.com>
2025-04-12 10:34:42 +0700

    init app

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-white dark:bg-black"
+    >
+      <Text className="text-5xl text-accent font-bold">Hello world</Text>
+    </View>
+  );
+}
